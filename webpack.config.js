1d10c0143cbefa17e305efd59a1ff88e1caa7063
viking zhang <viking_zhang@apple.com>
2016-01-27 17:29:36 +0800

    enable sourcemap for scss

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loaders: ['style', 'css', 'sass'],
+        loaders: ['style', 'css?sourceMap', 'sass?sourceMap'],
         include: APP_PATH
       },
       {
